873a9d9435e49c14a0efcc95d9c8cd70b15914a4
Ahmad Ardiansyah <ardiansyah3ber@gmail.com>
2023-07-04 21:11:32 +0700

    use pnpm all

--- src/components/NavbarTop.tsx
+++ src/components/NavbarTop.tsx
@@ -6,7 +6,7 @@ export default function NavbarTop() {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">Arralin</a>
+        <Link href={`/`} className="btn btn-ghost normal-case text-xl">Arralin</Link>
       </div>
       <div className="flex-none gap-2">
         <div className="form-control">
